61eeea110efed375847c5596f9ef771cdc4e6830
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-13 20:41:02 -0600

    remove app.css file

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,8 @@
 import { Router } from "@solidjs/router"
 import { FileRoutes } from "@solidjs/start"
 import { Suspense } from "solid-js"
+import "tailwindcss/tailwind.css"
 import Nav from "~/components/Nav"
-import "./app.css"
 
 export default function App() {
 	return (
