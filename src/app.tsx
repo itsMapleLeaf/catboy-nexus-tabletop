056526bcec64cf96972a58be25781b8d72b9b81e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-14 00:28:57 -0600

    improve twc + related surrounding config

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,19 @@
 // @refresh reload
 import { Router } from "@solidjs/router"
 import { FileRoutes } from "@solidjs/start"
-import { Suspense } from "solid-js"
+import { Suspense, createSignal } from "solid-js"
 import "tailwindcss/tailwind.css"
 import { Panel } from "./components/Panel.tsx"
 
 export default function App() {
+	const [isBlue, setIsBlue] = createSignal(false)
 	return (
 		<main class="relative h-dvh bg-[size:40px_40px] bg-repeat bg-grid-10 bg-grid-theme-border/40">
 			<Panel
 				as="button"
-				class="absolute left-10 top-10 flex size-20 flex-col items-center justify-center rounded-md transition-[filter] hover:brightness-110 active:brightness-125 active:transition-none"
+				class={`absolute left-10 top-10 flex size-20 flex-col items-center justify-center rounded-md transition-[filter] hover:brightness-110 active:brightness-125 active:transition-none ${isBlue() ? "bg-blue-500" : "bg-red-500"}`}
 				onClick={() => {
-					console.log("test")
+					setIsBlue(!isBlue())
 				}}
 			>
 				test
--- src/twc.tsx
+++ src/twc.tsx
@@ -2,47 +2,51 @@ import { Component, ComponentProps, JSX } from "solid-js"
 import { Dynamic } from "solid-js/web"
 import { twMerge } from "tailwind-merge"
 
-export const twc = new Proxy(
-	{},
-	{
-		get: (_, tag) => {
-			if (typeof tag !== "string") {
-				throw new Error("Tag must be a string")
-			}
-			return (strings: TemplateStringsArray, ...values: string[]) => {
-				return (props: {
-					class?: string
-					as?: keyof JSX.IntrinsicElements | Component
-				}) => {
-					const className = () => {
-						const classes = []
-						for (let i = 0; i < strings.length; i++) {
-							classes.push(strings[i])
-							if (i < values.length) {
-								classes.push(values[i])
-							}
-						}
-						classes.push(props.class)
-						return twMerge(classes)
-					}
-					return (
-						<Dynamic
-							component={props.as ?? tag}
-							{...props}
-							class={className()}
-						/>
-					)
-				}
-			}
-		},
-	},
-) as Twc
+type ValidComponentStrict = keyof JSX.IntrinsicElements | Component
 
-type Twc = {
-	[K in keyof JSX.IntrinsicElements]: (
-		strings: TemplateStringsArray,
-		...values: string[]
-	) => <As extends keyof JSX.IntrinsicElements | Component = K>(
-		props: ComponentProps<As> & { as?: As },
-	) => JSX.Element
+interface TwcComponentFactory<DefaultTag extends ValidComponentStrict> {
+	(strings: TemplateStringsArray, ...values: string[]): TwcComponent<DefaultTag>
 }
+
+interface TwcComponent<DefaultTag extends ValidComponentStrict> {
+	<As extends ValidComponentStrict = DefaultTag>(
+		props: ComponentProps<As> & { as?: As; class?: string },
+	): JSX.Element
+}
+
+function createComponentFactory<DefaultTag extends ValidComponentStrict>(
+	defaultTag: DefaultTag,
+): TwcComponentFactory<DefaultTag> {
+	return function createComponent(strings, ...values) {
+		return function TwcComponent(props) {
+			function* classes() {
+				for (let i = 0; i < strings.length - 1; i += 1) {
+					yield strings[i]
+					yield values[i]
+				}
+				yield strings.at(-1)
+				yield props.class
+			}
+			return (
+				<Dynamic
+					component={props.as ?? defaultTag}
+					{...props}
+					class={twMerge(...classes())}
+				/>
+			)
+		}
+	}
+}
+
+type Twc = typeof createComponentFactory & {
+	[Tag in keyof JSX.IntrinsicElements]: TwcComponentFactory<Tag>
+}
+
+export const twc = new Proxy(createComponentFactory, {
+	get: (_, defaultTag) => {
+		if (typeof defaultTag !== "string") {
+			throw new Error("Tag must be a string")
+		}
+		return createComponentFactory(defaultTag as keyof JSX.IntrinsicElements)
+	},
+}) as Twc
