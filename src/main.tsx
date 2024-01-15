845cf82f6e12e6329dfeeae7b9701942ad0873df
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-15 17:10:10 -0600

    add development indicator to title

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,8 @@
 import { render } from "solid-js/web"
 import { App } from "./App.tsx"
 
+if (process.env.NODE_ENV === "development") {
+	document.title += " [dev]"
+}
+
 render(() => <App />, document.getElementById("app") as HTMLElement)
