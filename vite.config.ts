4b6e431b8d75a50ee70201e7b05df21d4de4e00a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-15 16:49:37 -0600

    vercel plugin bad?

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite"
 import solid from "vite-plugin-solid"
-import vercel from "vite-plugin-vercel"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	plugins: [solid(), tsconfigPaths(), vercel()],
+	plugins: [solid(), tsconfigPaths()],
 	css: {
 		devSourcemap: true,
 	},
