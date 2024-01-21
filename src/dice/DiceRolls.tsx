296ecd8ec7a2ebcf55ae260143c37ab795fa2c65
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-21 13:32:37 -0600

    extract test rolls

--- src/dice/DiceRolls.tsx
+++ src/dice/DiceRolls.tsx
@@ -6,25 +6,27 @@ import { GenesysDiceRollSummary } from "../genesys/GenesysDiceRollSummary.tsx"
 import { Button } from "../ui/Button.tsx"
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/Popover.tsx"
 
+const testRolls = [...Array(100)].map((_, index) => ({
+	key: `${index}`,
+	caption: "test roll lol",
+	dice: [
+		{ die: genesysDice.proficiency, face: 1 },
+		{ die: genesysDice.ability, face: 2 },
+		{ die: genesysDice.boost, face: 3 },
+		{ die: genesysDice.challenge, face: 4 },
+		{ die: genesysDice.difficulty, face: 5 },
+		{ die: genesysDice.setback, face: 6 },
+	].map((die, index) => ({ ...die, key: index.toString() })),
+	rolledBy: "someone",
+	rolledAt: Date.now(),
+}))
+
 export function DiceRolls() {
 	return (
 		<div className="flex h-full flex-col gap-2 p-2">
 			<div className="flex min-h-0 flex-1 flex-col overflow-y-auto">
 				<Virtuoso
-					data={[...Array(100)].map((_, index) => ({
-						key: `${index}`,
-						caption: "test roll lol",
-						dice: [
-							{ die: genesysDice.proficiency, face: 1 },
-							{ die: genesysDice.ability, face: 2 },
-							{ die: genesysDice.boost, face: 3 },
-							{ die: genesysDice.challenge, face: 4 },
-							{ die: genesysDice.difficulty, face: 5 },
-							{ die: genesysDice.setback, face: 6 },
-						].map((die, index) => ({ ...die, key: index.toString() })),
-						rolledBy: "someone",
-						rolledAt: Date.now(),
-					}))}
+					data={testRolls}
 					itemContent={(_index, data) => (
 						<div className="pb-2">
 							<GenesysDiceRollSummary key={data.key} diceRoll={data} />
