cf04297c717cc43b3e2ac178e5666446b51ca0f3
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-26 01:32:42 -0600

    use vite again

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev"
+import { remixDevTools } from "remix-development-tools/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import remixConfig from "./remix.config.js"
 
 export default defineConfig({
-	plugins: [remix(remixConfig), tsconfigPaths()],
+	plugins: [remixDevTools(), remix(remixConfig), tsconfigPaths()],
 	css: {
 		devSourcemap: true,
 	},
