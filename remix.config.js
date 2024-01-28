b97a06728bd7690894bef33a287c462caf467c02
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-01-28 16:59:36 -0600

    use remix vite build in prod (lol (lmao))

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-/** @satisfies {import("@remix-run/dev").AppConfig} */
-export default {
-	appDirectory: "src",
-	future: {
-		v3_fetcherPersist: true,
-		v3_relativeSplatPath: true,
-	},
-}
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,32 @@ import { unstable_vitePlugin as remix } from "@remix-run/dev"
 import { remixDevTools } from "remix-development-tools/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
-import remixConfig from "./remix.config.js"
 
 export default defineConfig({
-	plugins: [remixDevTools(), remix(remixConfig), tsconfigPaths()],
+	plugins: [
+		remixDevTools(),
+		remix({
+			appDirectory: "src",
+			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+			},
+		}),
+		tsconfigPaths(),
+	],
 	css: {
 		devSourcemap: true,
 	},
 	ssr: {
 		noExternal: ["@clerk/clerk-react"],
+	},
+	build: {
+		rollupOptions: {
+			onwarn(warning, next) {
+				// Suppress warnings about "use client" directives
+				if (warning.message.includes("use client")) return
+				next(warning)
+			},
+		},
 	},
 })
