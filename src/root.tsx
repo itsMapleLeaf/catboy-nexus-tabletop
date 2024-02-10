e2da2e751314bb530056707b683b1ddf68b580d1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-09 23:15:14 -0600

    upgrades

--- src/root.tsx
+++ src/root.tsx
@@ -1,47 +1,21 @@
-import "@fontsource/barlow/latin-300.css"
-import "@fontsource/barlow/latin-400.css"
-import "@fontsource/barlow/latin-500.css"
-import "tailwindcss/tailwind.css"
-
-import {
-	ClerkApp,
-	ClerkErrorBoundary,
-	type RemixClerkProviderProps,
-} from "@clerk/remix"
+import { ClerkApp, ClerkErrorBoundary } from "@clerk/remix"
 import { rootAuthLoader } from "@clerk/remix/ssr.server"
 import { dark } from "@clerk/themes"
+import barlow300 from "@fontsource/barlow/latin-300.css?url"
+import barlow400 from "@fontsource/barlow/latin-400.css?url"
+import barlow500 from "@fontsource/barlow/latin-500.css?url"
 import type { LoaderFunctionArgs } from "@remix-run/node"
 import {
 	Links,
-	LiveReload,
 	Meta,
 	Outlet,
 	Scripts,
 	ScrollRestoration,
 } from "@remix-run/react"
+import tailwind from "tailwindcss/tailwind.css?url"
 import background from "./assets/bg.png"
 import logo from "./assets/logo-filled-light.svg"
 import { colors } from "./ui/theme.ts"
-
-const clerkAppearance: RemixClerkProviderProps["appearance"] = {
-	baseTheme: dark,
-	variables: {
-		borderRadius: "0.125rem",
-		colorAlphaShade: colors.copy,
-		colorBackground: colors.background,
-		colorInputBackground: colors.foreground,
-		colorInputText: colors.copy,
-		colorPrimary: colors["primary-light"],
-		colorText: colors.copy,
-		colorTextOnPrimaryBackground: colors["primary-content"],
-		colorTextSecondary: colors["copy-lighter"],
-	},
-	elements: {
-		card: {
-			boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)",
-		},
-	},
-}
 
 export async function loader(args: LoaderFunctionArgs) {
 	return rootAuthLoader(args)
@@ -59,6 +33,10 @@ function Root() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<title>{`Catboy Nexus${process.env.NODE_ENV === "development" ? " [dev]" : ""}`}</title>
 				<link rel="icon" href={logo} />
+				<link rel="stylesheet" href={barlow300} />
+				<link rel="stylesheet" href={barlow400} />
+				<link rel="stylesheet" href={barlow500} />
+				<link rel="stylesheet" href={tailwind} />
 				<Meta />
 				<Links />
 			</head>
@@ -68,12 +46,31 @@ function Root() {
 				</div>
 				<ScrollRestoration />
 				<Scripts />
-				<LiveReload />
 			</body>
 		</html>
 	)
 }
 
-export default ClerkApp(Root, { appearance: clerkAppearance })
+export default ClerkApp(Root, {
+	appearance: {
+		baseTheme: dark,
+		variables: {
+			borderRadius: "0.125rem",
+			colorAlphaShade: colors.copy,
+			colorBackground: colors.background,
+			colorInputBackground: colors.foreground,
+			colorInputText: colors.copy,
+			colorPrimary: colors["primary-light"],
+			colorText: colors.copy,
+			colorTextOnPrimaryBackground: colors["primary-content"],
+			colorTextSecondary: colors["copy-lighter"],
+		},
+		elements: {
+			card: {
+				boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)",
+			},
+		},
+	},
+})
 
 export const ErrorBoundary = ClerkErrorBoundary()
