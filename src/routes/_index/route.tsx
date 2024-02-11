0b07bc2d33fbf2562932d3d1aec0656dedceac01
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-11 12:39:01 -0600

    require login for new room

--- src/routes/_index/route.tsx
+++ src/routes/_index/route.tsx
@@ -1,4 +1,5 @@
-import { LucideWand2 } from "lucide-react"
+import { SignInButton, UserButton, useUser } from "@clerk/remix"
+import { LucideLogIn, LucideWand2 } from "lucide-react"
 import logo from "~/assets/logo.svg"
 import { Button } from "~/ui/Button.tsx"
 import { ExternalLink } from "~/ui/ExternalLink.tsx"
@@ -6,36 +7,64 @@ import { Link } from "~/ui/Link.tsx"
 import { PromptButton } from "~/ui/PromptButton.tsx"
 
 export default function Home() {
+	const { isLoaded, isSignedIn } = useUser()
 	return (
 		<div className="flex min-h-dvh flex-col items-center">
-			<div className="flex-1" />
-			<main className="flex-col-center gap-6 p-6  text-center">
+			<header className="flex w-full flex-1 flex-col p-3">
+				<div className="ml-auto opacity-70 transition focus-within:opacity-100 hover:opacity-100">
+					<UserButton />
+				</div>
+			</header>
+			<main className="flex-col-center gap-6 p-6 text-center">
 				<header className="flex items-center justify-center gap-3 drop-shadow-[0_0_4px_rgba(0,0,0,1)]">
 					<img src={logo} alt="" className="h-16" />
 					<h1 className="text-5xl font-light">Catboy Nexus</h1>
 				</header>
+
 				<p className="max-w-screen-md text-2xl drop-shadow-[0_0_3px_rgba(0,0,0,1)]">
 					Welcome! Catboy Nexus is a work in progress virtual tabletop for
 					rules-lite and rules-medium game systems. Currently supports Genesys,
 					with others planned later.
 				</p>
-				<PromptButton
-					render={<Button size="xl" appearance="solid" />}
-					title="New Room"
-					description="Create a new room to play in."
-					label="Room Name"
-					placeholder="Call it something cool! Or something boring. I'm not your boss."
-					confirmText="Create"
-					confirmIcon={<LucideWand2 />}
-					onSubmit={(value) => {
-						console.log(value)
-					}}
+
+				<div
+					className="flex-col-center h-[5.25rem] opacity-0 transition data-[loaded=true]:opacity-100"
+					data-loaded={isLoaded}
 				>
-					<LucideWand2 /> New Room
-				</PromptButton>
+					{!isLoaded ? null : isSignedIn ? (
+						<PromptButton
+							title="New Room"
+							description="Create a new room to play in."
+							label="Room Name"
+							placeholder="Call it something cool! Or something boring. I'm not your boss."
+							confirmText="Create"
+							confirmIcon={<LucideWand2 />}
+							onSubmit={(value) => {
+								console.log(value)
+							}}
+							render={<Button size="xl" appearance="solid" />}
+						>
+							<LucideWand2 /> New Room
+						</PromptButton>
+					) : (
+						<>
+							<SignInButton mode="modal">
+								<Button
+									size="xl"
+									appearance="solid"
+									className="animate-in fade-in"
+								>
+									<LucideLogIn /> Sign In
+								</Button>
+							</SignInButton>
+							<p className="mt-auto">Sign in to keep track of your rooms.</p>
+						</>
+					)}
+				</div>
 			</main>
+
 			<footer className="flex flex-1 flex-col items-center justify-end">
-				<p className="p-2 text-center text-sm">
+				<p className="p-2 text-center text-sm text-theme-copy/70">
 					Powered by{" "}
 					<Link as={ExternalLink} href="https://convex.dev">
 						Convex
--- src/ui/Link.tsx
+++ src/ui/Link.tsx
@@ -2,5 +2,5 @@ import { classed } from "~/ui/classed.ts"
 
 export const Link = classed(
 	"a",
-	"text-theme-primary-content underline hover:no-underline",
+	"cursor-pointer text-theme-primary-content underline hover:no-underline",
 )
