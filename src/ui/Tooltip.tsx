ddc80c8c93954e7d12bb358e0d85f00d6d920531
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-11 22:23:26 -0600

    origin aware tooltip animation

--- src/ui/Tooltip.tsx
+++ src/ui/Tooltip.tsx
@@ -2,17 +2,26 @@ import * as Ariakit from "@ariakit/react"
 
 export function Tooltip({
 	tooltip,
-	placement,
+	placement = "top",
 	...props
 }: Ariakit.TooltipAnchorProps & {
 	tooltip: React.ReactNode
 	placement?: Ariakit.TooltipProviderProps["placement"]
 }) {
+	const originClass = placement.startsWith("top")
+		? "origin-bottom"
+		: placement.startsWith("right")
+			? "origin-left"
+			: placement.startsWith("bottom")
+				? "origin-top"
+				: placement.startsWith("left")
+					? "origin-right"
+					: "origin-bottom"
 	return (
 		<Ariakit.TooltipProvider placement={placement}>
 			<Ariakit.TooltipAnchor {...props} />
 			<Ariakit.Tooltip
-				className="pointer-events-none translate-y-0.5 rounded bg-theme-copy px-1.5 py-1 text-sm font-medium leading-none text-theme-background opacity-0 transition data-[enter]:translate-y-0 data-[enter]:opacity-100"
+				className={`pointer-events-none scale-90 rounded bg-theme-copy px-1.5 py-1 text-sm font-medium leading-none text-theme-background opacity-0 transition data-[enter]:scale-100 data-[enter]:opacity-100 ${originClass}`}
 				gutter={4}
 				unmountOnHide
 			>
