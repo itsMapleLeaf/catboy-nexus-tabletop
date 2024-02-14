df529cd89e2ce9e998d33a19f9e386f1e27142d5
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-13 23:09:42 -0600

    i'm extremely smart

--- src/helpers/context.tsx
+++ src/helpers/context.tsx
@@ -1,20 +1,13 @@
 import * as React from "react"
-import { createContext } from "react"
 import { raise } from "./errors.ts"
 
 export function defineContext<T>() {
-	const Context = createContext<T | undefined>(undefined)
-
-	function Provider(props: { children: React.ReactNode; value: T }) {
-		return (
-			<Context.Provider value={props.value}>{props.children}</Context.Provider>
-		)
-	}
+	const Context = React.createContext<T | undefined>(undefined)
 
 	function useContextValue() {
 		const value = React.useContext(Context)
 		return value ?? raise("useContextValue must be used within a Provider")
 	}
 
-	return [Provider, useContextValue] as const
+	return [Context.Provider, useContextValue] as const
 }
