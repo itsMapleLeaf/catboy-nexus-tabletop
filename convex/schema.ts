bbfa7bf0e6e3419cd7ef3384a42538ed2c478280
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-16 12:20:40 -0600

    dice in db

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,11 @@
 import { defineSchema, defineTable } from "convex/server"
-import { v } from "convex/values"
+import { type Infer, v } from "convex/values"
+
+const rolledDieValidator = v.object({
+	type: v.id("diceTypes"),
+	face: v.number(),
+})
+export type RolledDie = Infer<typeof rolledDieValidator>
 
 export default defineSchema({
 	profiles: defineTable({
@@ -19,4 +25,41 @@ export default defineSchema({
 	})
 		.index("by_room", ["roomId"])
 		.index("by_collection", ["collectionName"]),
+
+	images: defineTable({
+		slug: v.string(),
+		storageId: v.string(),
+	}).index("by_slug", ["slug"]),
+
+	diceTypes: defineTable({
+		name: v.string(),
+		color: v.optional(
+			v.union(
+				v.literal("red"),
+				v.literal("orange"),
+				v.literal("yellow"),
+				v.literal("green"),
+				v.literal("blue"),
+				v.literal("purple"),
+				v.literal("white"),
+				v.literal("black"),
+			),
+		),
+		faces: v.array(
+			v.object({
+				symbols: v.array(
+					v.union(
+						v.object({ type: v.literal("text"), value: v.string() }),
+						v.object({ type: v.literal("image"), imageSlug: v.string() }),
+					),
+				),
+			}),
+		),
+	}),
+
+	diceRolls: defineTable({
+		roomId: v.id("rooms"),
+		rolledDice: v.array(rolledDieValidator),
+		rolledBy: v.id("users"),
+	}),
 })
