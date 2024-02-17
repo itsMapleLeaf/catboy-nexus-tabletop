e1a24e473438986bdbbc72b6fcfe4189c5b08d0c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-17 02:28:18 -0600

    improved auth tests

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -1,19 +1,31 @@
 import { expect, test } from "@playwright/test"
-import { signIn, signOut } from "./helpers/auth.ts"
+import { authStoragePath } from "./helpers/auth.ts"
 
-test("auth redirects", async ({ page, context }) => {
+test("redirects to landing when signed out", async ({ page }) => {
 	await page.goto("/")
-
-	await expect(page.getByTestId("landing-title")).toBeVisible()
-	await expect(page.getByText("Sign In")).toBeVisible()
-
-	await signIn(page)
-
-	await page.goto("/")
-	await page.waitForURL("/rooms")
-
-	await signOut(context, page)
-
-	await page.reload()
 	await page.waitForURL("/")
+	await expect(page.getByTestId("landing-title")).toBeVisible()
+
+	await page.goto("/rooms")
+	await page.waitForURL("/")
+	await expect(page.getByTestId("landing-title")).toBeVisible()
+})
+
+test.describe("when signed in", () => {
+	test.use({ storageState: authStoragePath })
+
+	test("redirects to room list", async ({ page, context }) => {
+		await page.goto("/")
+		await page.waitForURL("/rooms")
+
+		await page.goto("/rooms")
+		await page.waitForURL("/rooms")
+	})
+
+	test("signing out leads to landing", async ({ page }) => {
+		await page.goto("/rooms")
+		await page.getByLabel("Open user button").click()
+		await page.getByRole("menuitem", { name: "Sign out" }).click()
+		await page.waitForURL("/")
+	})
 })
