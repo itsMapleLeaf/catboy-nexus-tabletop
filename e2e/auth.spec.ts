20c5caaa1b0dbc581934666c3da1d3271f9355fc
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-17 02:45:51 -0600

    isolate session for signout test

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "@playwright/test"
-import { authStoragePath } from "./helpers/auth.ts"
+import { authStoragePath, signIn } from "./helpers/auth.ts"
 
 test("redirects to landing when signed out", async ({ page }) => {
 	await page.goto("/")
@@ -21,8 +21,13 @@ test.describe("when signed in", () => {
 		await page.goto("/rooms")
 		await page.waitForURL("/rooms")
 	})
+})
 
-	test("signing out leads to landing", async ({ page }) => {
+// use a separate session for testing signout,
+// so we don't invalidate the token for other tests
+test.describe("when signing out", () => {
+	test("redirects to landing", async ({ page }) => {
+		await signIn(page)
 		await page.goto("/rooms")
 		await page.getByLabel("Open user button").click()
 		await page.getByRole("menuitem", { name: "Sign out" }).click()
